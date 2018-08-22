59beb4d8fbff4a8d7221008eb0fdcde92290c363
RCode-Blue <rickyliew@hotmail.com>
2018-08-22 11:36:28 +1000

    1-11 Render Targets

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
+// Get React and ReactDOM:
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// Create new component. 
+// This component should produce some HTML
+// WHAT
+const App = () => {
+    return <div>Hi!</div>;  // JSX
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take this component's generated HTML and put it
+// on the page (in the DOM)
+// WHERE
+ReactDOM.render(<App />, document.querySelector('.container')); // Creates an instance of App and renders it in the 'container' div
