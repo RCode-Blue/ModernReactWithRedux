acd51fe07e5419153452ef8f28a070025fcbd961
RCode-Blue <rickyliew@hotmail.com>
2018-08-22 14:55:57 +1000

    1-19: Controlled Components-fixed typo

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -20,8 +20,10 @@ class SearchBar extends Component{
       // Controlled component
       <div>
         <input 
-          value = {this.state.term}                                             {/* 02 upon rendering, value gets updated to new value of this.state.term */}
-          onChange = {event => this.setState({ term: event.target.value })} />  {/* 01 re-renders when there is a change */}
+          // 02 upon rendering, value gets updated to new value of this.state.term
+          value = {this.state.term}
+          // 01 re-renders when there is a change
+          onChange = {event => this.setState({ term: event.target.value })} />  
       </div>
     );
   }
