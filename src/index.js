bf61b3b99815fa5b4a9a70e906e2d785e6360677
RCode-Blue <rickyliew@hotmail.com>
2018-08-22 16:30:18 +1000

    2-23: Props

--- src/index.js
+++ src/index.js
@@ -1,32 +1,37 @@
 // Get React and ReactDOM:
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list'
 
 const API_KEY = '';
 // const API_KEY = process.env.REACT_APP_YOUTUBE_KEY;
 
-YTSearch({key: API_KEY, term: 'surfboards'}, 
-    function(data){
-        console.log(data)
-    }
-);
 
-// Create new component. 
-// This component should produce some HTML
-// WHAT
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );  // JSX
+
+class App extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = { videos: {} };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+                this.setState({ videos });   // Same as: this.setState({ videos: videos });
+            }
+        );
+    }
+    render() {
+        return(
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 
-// Take this component's generated HTML and put it
-// on the page (in the DOM)
-// WHERE
+
 ReactDOM.render(<App />, document.querySelector('.container')); // Creates an instance of App and renders it in the 'container' div
