a3a3a5acd8037bec0a8e01fbea72cff7733f591c
RCode-Blue <rickyliew@hotmail.com>
2018-08-22 14:15:11 +1000

    1-16: User Events, Arrow Functions

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,8 +9,15 @@ import React, { Component } from 'react';
 // Class-based component
 class SearchBar extends Component{
   render(){
-    return <input />;
+    // return <input onChange = {this.onInputChange} />;
+    // return <input onChange = {(event) => console.log(event.target.value)} />;
+    return <input onChange = {event => console.log(event.target.value)} />;
   }
+
+  // event handler
+  // onInputChange(event){
+  //   console.log(event.target.value);
+  // }
 }
 
 
