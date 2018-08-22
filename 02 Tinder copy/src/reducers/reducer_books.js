cfa59d6e8851f0ce145b32bd9c86edcfcce64f94
RCode-Blue <rickyliew@hotmail.com>
2018-08-22 10:41:10 +1000

    Start over

--- 02 Tinder copy/src/reducers/reducer_books.js
+++
@@ -1,8 +0,0 @@
-export default function() {
-    return [
-        {title: 'Javascript: The Good Parts'},
-        {title: 'Harry Potter and the Prisoner of Azkaban'},
-        {title: 'The Dark Tower'},
-        {title: 'Eloquent Ruby'}
-    ];
-}
