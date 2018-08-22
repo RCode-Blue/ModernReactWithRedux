7853d449f72cd3070164551dc64b00d8abcc03a7
RCode-Blue <rickyliew@hotmail.com>
2018-08-22 15:45:36 +1000

    2-1 Reverted

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 // Get React and ReactDOM:
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
-const API_KEY = '';
+const API_KEY = 'AIzaSyCx0qCEcEZcEiCXIGap1jO3Qo9yPpSR3pA';
 // const API_KEY = process.env.REACT_APP_YOUTUBE_KEY;
+
+YTSearch({key: API_KEY, term: 'surfboards'},
+    function(data){
+        console.log(data);
+    }
+);
 
 // Create new component. 
 // This component should produce some HTML
