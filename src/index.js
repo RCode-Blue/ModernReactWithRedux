fd18723fe4a5ed5577e147713d70a3aa5b04ed50
RCode-Blue <rickyliew@hotmail.com>
2018-08-22 15:56:51 +1000

    2-1: Youtube Search text

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
-const API_KEY = 'AIzaSyCx0qCEcEZcEiCXIGap1jO3Qo9yPpSR3pA';
+const API_KEY = '';
 // const API_KEY = process.env.REACT_APP_YOUTUBE_KEY;
 
-YTSearch({key: API_KEY, term: 'surfboards'},
+YTSearch({key: API_KEY, term: 'surfboards'}, 
     function(data){
-        console.log(data);
+        console.log(data)
     }
 );
 
