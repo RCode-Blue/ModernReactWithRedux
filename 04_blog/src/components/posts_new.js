92665ac5eaa15843fffdf113cc702f9c205c2b91
RCode-Blue <rickyliew@hotmail.com>
2018-08-28 16:51:16 +1000

    9-137: handlesubmit

--- 04_blog/src/components/posts_new.js
+++ 04_blog/src/components/posts_new.js
@@ -11,24 +11,31 @@ class PostsNew extends Component{
           type="text"
           {...field.input}
         />
+        {field.meta.error}
       </div>
     );
   }
 
-
+  onSubmit(values){
+    console.log(values);
+    
+  }
 
 
   render(){
+    const { handleSubmit } = this.props;
+
+
     return(
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
           label="Title"
           name="title"
           component={this.renderField}
         />
         <Field
-          label="Tags"
-          name="tags"
+          label="Categories"
+          name="categories"
           component={this.renderField}
         />
         <Field
@@ -36,11 +43,33 @@ class PostsNew extends Component{
           name="content"
           component={this.renderField}
         />
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+
       </form>
     );
   }
 }
 
+function validate(values){
+  // console.log(values) -> title: 'srg', categories:'sgere', content: 'hrhj'
+  const errors = {};
+
+  if(!values.title){
+    errors.title = 'Enter a title';
+  }
+  if(!values.categories){
+    errors.categories = 'Enter some categories';
+  }
+  if(!values.content){
+    errors.content = 'Enter some content please';
+  }
+
+  // if errors is empty, thge form is fine to submit
+  return errors;
+}
+
 export default reduxForm({
+  validate: validate,
   form: 'PostsNewForm'
 })(PostsNew);
